01bcc59d35e24c70c51afed9aba1d3b766063746
Valentin Kostin <Kostin.icon@yandex.ru>
2026-08-16 16:32:54 +0300

    na russkom 002

--- ocr_app/engine/kraken_engine.py
+++ ocr_app/engine/kraken_engine.py
@@ -31,14 +31,7 @@ class KrakenEngine(OCREngine):
             import kraken
             
             # Попытаться получить версию для проверки установки
-            try:
-                self._kraken_version = kraken.__version__
-            except AttributeError:
-                from importlib.metadata import version as get_package_version
-                try:
-                    self._kraken_version = get_package_version("kraken")
-                except Exception:
-                    self._kraken_version = "7.1"
+            self._kraken_version = kraken.__version__
             
             # Получить список установленных моделей (может завершиться ошибкой, если модели не установлены)
             try:
